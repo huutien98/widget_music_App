ab144af7178a9163360d52183a32fe5987448a3f
huuti <dakietkhac007@gmail.com>
2020-10-20 19:07:39 +0700

    Initial commit

---
+++ app/src/main/java/com/shinmusic/Model/BaiHat.java
@@ -0,0 +1,125 @@
+package com.shinmusic.Model;
+
+import android.content.ContentUris;
+import android.net.Uri;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class BaiHat implements Parcelable {
+
+@SerializedName("IdBaiHat")
+@Expose
+private String idBaiHat;
+@SerializedName("TenBaiHat")
+@Expose
+private String tenBaiHat;
+@SerializedName("CaSy")
+@Expose
+private String caSy;
+@SerializedName("ImageBaiHat")
+@Expose
+private String imageBaiHat;
+@SerializedName("DuongDan")
+@Expose
+private String duongDan;
+@SerializedName("LuotThich")
+@Expose
+private String luotThich;
+
+    public BaiHat(Parcel in) {
+        idBaiHat = in.readString();
+        tenBaiHat = in.readString();
+        caSy = in.readString();
+        imageBaiHat = in.readString();
+        duongDan = in.readString();
+        luotThich = in.readString();
+    }
+
+    public static final Creator<BaiHat> CREATOR = new Creator<BaiHat>() {
+        @Override
+        public BaiHat createFromParcel(Parcel in) {
+            return new BaiHat(in);
+        }
+
+        @Override
+        public BaiHat[] newArray(int size) {
+            return new BaiHat[size];
+        }
+    };
+
+    public BaiHat(String id, String title, String artist) {
+        this.idBaiHat = id;
+        this.tenBaiHat = title;
+        this.caSy = artist;
+
+    }
+
+    public String getIdBaiHat() {
+return idBaiHat;
+}
+
+public void setIdBaiHat(String idBaiHat) {
+this.idBaiHat = idBaiHat;
+}
+
+public String getTenBaiHat() {
+return tenBaiHat;
+}
+
+public void setTenBaiHat(String tenBaiHat) {
+this.tenBaiHat = tenBaiHat;
+}
+
+public String getCaSy() {
+return caSy;
+}
+
+public void setCaSy(String caSy) {
+this.caSy = caSy;
+}
+
+public String getImageBaiHat() {
+return imageBaiHat;
+}
+
+public void setImageBaiHat(String imageBaiHat) {
+this.imageBaiHat = imageBaiHat;
+}
+
+public String getDuongDan() {
+return duongDan;
+}
+
+public void setDuongDan(String duongDan) {
+this.duongDan = duongDan;
+}
+
+public String getLuotThich() {
+return luotThich;
+}
+
+public void setLuotThich(String luotThich) {
+this.luotThich = luotThich;
+}
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(idBaiHat);
+        parcel.writeString(tenBaiHat);
+        parcel.writeString(caSy);
+        parcel.writeString(imageBaiHat);
+        parcel.writeString(duongDan);
+        parcel.writeString(luotThich);
+    }
+    public Uri getURI() {
+        return ContentUris.withAppendedId(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, Long.parseLong(idBaiHat));
+    }
+}
